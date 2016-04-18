c76b5b391b31efc64e79cdb93e871be4ca550f9f
James Bradley <hintonbradley@gmail.com>
2016-04-18 10:20:18 -0700

    Updated port

--- app.js
+++ app.js
@@ -46,5 +46,9 @@ app.post('/users', function(req,res) {
   });
 });
 
-app.listen(process.env.PORT || 3000)
+// app.listen(process.env.PORT || 3000)
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
